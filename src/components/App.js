7917b1a74a8795b23d3f30b7f22598f40371ff44
Jimmy Mike Okwiri <jimmy.okwiri@student.moringaschool.com>
2025-07-29 15:33:26 +0300

    Finishes lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
+import QuestionItem from "./QuestionItem";
 import QuestionList from "./QuestionList";
 
 function App() {
-  const [page, setPage] = useState("List");
+	const [page, setPage] = useState("List");
+	const [questions, setQuestions] = useState([]);
 
-  return (
-    <main>
-      <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
-    </main>
-  );
+	useEffect(() => {
+		fetch("http://localhost:4000/questions")
+			.then((res) => res.json())
+			.then((questions) => setQuestions(questions));
+	}, []);
+
+	function onNewQuestion(question) {
+		setQuestions([...questions, question]);
+	}
+
+	function onDeleteQuestion(deletedQuestion) {
+		const updatedQuestions = questions.filter((question) => question.id !== deletedQuestion.id);
+		setQuestions(updatedQuestions);
+	}
+
+	function onChangeAnswer(updatedQuestion) {
+		const updatedQuestions = questions.map((question) => {
+			if (updatedQuestion.id === question.id) {
+				return updatedQuestion;
+			} else {
+				return question;
+			}
+		});
+
+		setQuestions(updatedQuestions);
+	}
+
+	return (
+		<main>
+			<AdminNavBar onChangePage={setPage} />
+			{page === "Form" ? <QuestionForm onNewQuestion={onNewQuestion} /> : <QuestionList questions={questions} onDeleteQuestion={onDeleteQuestion} onChangeAnswer={onChangeAnswer}/>}
+		</main>
+	);
 }
 
 export default App;
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion, onChangeAnswer }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,38 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleClick(){
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(() => onDeleteQuestion(question))
+  }
+
+  function handleChange(e){
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        correctIndex: e.target.value
+      })
+    })
+    .then(res => res.json())
+    .then(question => onChangeAnswer(question))
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleClick}>Delete Question</button>
     </li>
   );
 }
